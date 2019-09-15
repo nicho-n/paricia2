221411dad266bec328dec2addd8dd30e8f2b4bcf
nicho-n <nicholaspnowak@gmail.com>
2019-09-15 00:16:22 -0400

    changed planet

--- pub/init.js
+++ pub/init.js
@@ -2,7 +2,7 @@ import * as THREE from "./three/three.module.js";
 import { EffectComposer } from "./jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "./jsm/postprocessing/RenderPass.js";
 import { stars } from "./stars.js";
-import { planet_1, planet_1_clouds } from "./planets/planet_1.js";
+import { planet_1 } from "./planets/planet_1.js";
 import { moon_1 } from "./planets/moon_1.js";
 import { FlyControls } from "./jsm/controls/FlyControls.js";
 
@@ -14,25 +14,24 @@ var SCREEN_WIDTH = window.innerWidth;
 
 function init() {
   renderer = new THREE.WebGLRenderer({ antialias: true });
-  dirLight = new THREE.DirectionalLight(0xffffff);
+  dirLight = new THREE.DirectionalLight(0xffffff, 0.4);
   camera = new THREE.PerspectiveCamera(25, screen.height / screen.width, 50, 1e7);
   controls = new FlyControls(camera);
   clock = new THREE.Clock();
-  
+
   scene = new THREE.Scene();
   scene.fog = new THREE.FogExp2(0x000000, 0.00000025);
   scene.add(dirLight);
   scene.add(planet_1);
-  scene.add(planet_1_clouds);
   scene.add(moon_1);
   scene.add(stars);
 
-  dirLight.position.set(-1, 0, 1).normalize
+  dirLight.position.set(-1, 0, 1000).normalize
 
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
   camera.position.z = 6540 * 5;
-  camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+  camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;-
   camera.updateProjectionMatrix();
 
   renderModel = new RenderPass(scene, camera);
@@ -59,7 +58,6 @@ function animate() {
 function render() {
   var delta = clock.getDelta();
   planet_1.rotation.y += rotationSpeed * delta;
-  planet_1_clouds.rotation.y += 1.25 * rotationSpeed * delta;
   position_indicator.innerHTML = camera.position.x + "," + camera.position.y + "," + camera.position.z;
   controls.update(delta);
   composer.render(delta);
--- pub/planets/planet_1.js
+++ pub/planets/planet_1.js
@@ -8,17 +8,8 @@ var cloudsScale = 1.005;
 var materialNormalMap = new THREE.MeshPhongMaterial({
   specular: 0x333333,
   shininess: 15,
-  map: textureLoader.load("../textures/planets/earth_atmos_2048.jpg"),
-  specularMap: textureLoader.load(
-    "../textures/planets/earth_specular_2048.jpg"
-  ),
-  normalMap: textureLoader.load("../textures/planets/earth_normal_2048.jpg"),
+  map: textureLoader.load("../textures/planets/mars-surface.jpg"),
   normalScale: new THREE.Vector2(0.85, 0.85)
-});
-
-var materialClouds = new THREE.MeshLambertMaterial({
-  map: textureLoader.load("../textures/planets/earth_clouds_1024.png"),
-  transparent: true
 });
 
 var geometry = new THREE.SphereBufferGeometry(radius, 100, 50);
@@ -26,9 +17,4 @@ var meshPlanet = new THREE.Mesh(geometry, materialNormalMap);
 meshPlanet.rotation.y = 0;
 meshPlanet.rotation.z = tilt;
 
-var meshClouds = new THREE.Mesh(geometry, materialClouds);
-meshClouds.scale.set(cloudsScale, cloudsScale, cloudsScale);
-meshClouds.rotation.z = tilt;
-
 export var planet_1 = meshPlanet;
-export var planet_1_clouds = meshClouds;
