23881f47e35e60f4dfec041f5f988e53b4c872a6
nicho-n <nicholaspnowak@gmail.com>
2019-09-22 01:22:27 -0400

    added server-side unit tests

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var app = express();
+var http = require("http");
+var server = http.Server(app);
+var socketio = require("socket.io");
+var io = socketio(server);
 
 app.use(express.static("pub"));
 
-app.listen(3000, function() {
+io.on('connection', function(socket) {
+  console.log("incoming connection")
+})
+
+
+
+
+server.listen(3000, function() {
   console.log("Listening on port 3000!");
 });
---
+++ spec/server_spec.js
@@ -0,0 +1,11 @@
+var socket = require('socket.io-client')('http://localhost:3000')
+
+describe('Connecting successfully and disconnecting', function () {
+
+    it('should form a connection', function (done) {
+      socket.on('connect', function() {
+        expect(socket.connected).toBe(true);
+        done();
+      })
+    });
+});
