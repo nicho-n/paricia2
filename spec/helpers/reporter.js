c89963b2ac70e4c55ea46fe14ec4eefc325291e9
nicho-n <nicholaspnowak@gmail.com>
2019-09-22 01:48:48 -0400

    reporter formatting

--- spec/helpers/reporter.js
+++ spec/helpers/reporter.js
@@ -1,8 +1,4 @@
 const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
-
 jasmine.getEnv().clearReporters();               // remove default reporter logs
-jasmine.getEnv().addReporter(new SpecReporter({  // add jasmine-spec-reporter
-  spec: {
-    displayPending: true
-  }
-}));
+jasmine.getEnv().addReporter(new SpecReporter({spec: {displayPending: true}}));  // add jasmine-spec-reporter
+  
