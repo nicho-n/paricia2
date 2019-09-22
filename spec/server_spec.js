fb2426cded457e87218fd75445dfdf264f06983e
nicho-n <nicholaspnowak@gmail.com>
2019-09-22 01:34:04 -0400

    add spec reporter

--- spec/server_spec.js
+++ spec/server_spec.js
@@ -1,7 +1,5 @@
 var socket = require('socket.io-client')('http://localhost:3000')
-
 describe('Connecting successfully and disconnecting', function () {
-
     it('should form a connection', function (done) {
       socket.on('connect', function() {
         expect(socket.connected).toBe(true);
