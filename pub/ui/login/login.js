7bddce5b53fdfce2d8e1447884dfe620d863bfaf
nick <nicholaspnowak@gmail.com>
2019-09-28 15:26:41 -0400

    fix whitespace

--- pub/ui/login/login.js
+++ pub/ui/login/login.js
@@ -3,7 +3,7 @@ var socket = io();
 function connect(){
     var username = document.getElementById("username-prompt").value;
     var password = document.getElementById("password-prompt").value;
-	socket.emit('login', {"username": username, "password": password});
+    socket.emit('login', {"username": username, "password": password});
 }
 
 socket.on('login ok', function(player) {
