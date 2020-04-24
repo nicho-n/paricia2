eca5253319ae1c3fb471320622be3f83c1eb24e7
nicho-n <nicholaspnowak@gmail.com>
2020-04-23 21:06:57 -0400

    update camera position

--- pub/3d/player/PlayerLoader.js
+++ pub/3d/player/PlayerLoader.js
@@ -3,9 +3,9 @@ var PlayerLoader = {
         var loader = new OBJLoader();
         player = loader.parse(player1.ship)
         player.add(camera);
-        player.scale.set(6,6,6);
+        player.scale.set(7,7,7);
         player.position.set(0,0,22890)
-        camera.position.set(0, 25, 250);   
+        camera.position.set(0, 17, 125);   
         controls = new THREE.FlyControls(player);    
         controls.movementSpeed = 1000;
         controls.rollSpeed = Math.PI / 24;
