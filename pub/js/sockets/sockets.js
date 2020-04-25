8cf5081e4df3fa86da6786e6f6cd9a303efd7f8b
nicho-n <nicholaspnowak@gmail.com>
2020-04-24 21:17:28 -0400

    remove console logs

--- pub/js/sockets/sockets.js
+++ pub/js/sockets/sockets.js
@@ -3,14 +3,12 @@ var socket = io();
 socket.on('player joined', function(newPlayer){
     if (player) {
         PlayerLoader.load(newPlayer);
-        console.log("adding join socket")    
     }
 });
 
 socket.on('online players', function(players){
    for(const other in players) {
         if (player.username != other.username) {
-            console.log("adding online players")
             PlayerLoader.load(other, true);
         }
    }
